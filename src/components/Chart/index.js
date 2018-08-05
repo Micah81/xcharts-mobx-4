13761d6257644ddcba6c3d32cdd9bcc408f9ffd6
Micah81 <micahmrocks@gmail.com>
2018-08-05 15:29:36 -0600

    Not yet calling the chartData correctly.

--- src/components/Chart/index.js
+++ src/components/Chart/index.js
@@ -1,21 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { inject, observer } from 'mobx-react';
 import { compose } from 'recompose';
 import { VictoryCandlestick } from 'victory';
 
-const CandlestickChart = ({ chartStore }) => (
-  <div>
-  <h1>Chart</h1>
-  {console.log(chartStore.chartData)}
-  <VictoryCandlestick
-    data={
-      chartStore.chartData
-    }
-    />
-  </div>
-);
+class CandlestickChart extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Chart</h1>
+        <VictoryCandlestick
+          data={this.chartStore}
+        />
+      </div>
+    )
+  }
+}
 
 export default compose(
   inject('chartStore'),
   observer
-)(CandlestickChart)
+)(CandlestickChart);
