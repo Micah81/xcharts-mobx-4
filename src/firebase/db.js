07b41bda4ccf34fe68b152fdf93ca1c442e0f53f
Micah81 <micahmrocks@gmail.com>
2018-08-08 21:42:39 -0600

    The up-vote records without duplicates.

--- src/firebase/db.js
+++ src/firebase/db.js
@@ -12,15 +12,41 @@ export const onceGetUsers = () =>
 
 // Voting API
 export const voteUp = (symbol, today, user) =>
-  db.ref('/votes/' +today+ '/' +symbol+ '/voters/').push({
-    user: user
-  }).then(() =>{
-    db.ref('/users/' +user+ '/votes/' +today+ '/').push({
-      symbol: symbol,
-      vote: 'up'
-    }).then(() => {
-      db.ref('/votes/' +today+ '/' +symbol+ '/upVotes').transaction(function(upVotes){
-        return upVotes + 1
+  db.ref('/votes/' +today+ '/' +symbol+ '/').child('voters').orderByChild('user').equalTo(user).on("value", function(snapshot) {
+    console.log(snapshot.val());
+
+    if(snapshot.val()==null){
+      db.ref('/votes/' +today+ '/' +symbol+ '/voters/').push({
+        user: user
+      }).then(() =>{
+        db.ref('/users/' +user+ '/votes/' +today+ '/').push({
+          symbol: symbol,
+          vote: 'up'
+        }).then(() => {
+          db.ref('/votes/' +today+ '/' +symbol+ '/upVotes').transaction(function(upVotes){
+            return upVotes + 1
+          })
+        })
       })
-    })
-  })
+    }
+
+    snapshot.forEach(function(data) {
+        if(data.key==null){
+          // Record the vote
+          db.ref('/votes/' +today+ '/' +symbol+ '/voters/').push({
+            user: user
+              }).then(()=>{
+               db.ref('/users/'+user+ '/votes/' + today+ '/').push({
+                 symbol: symbol,
+                 vote: 'up'
+               }).then(()=>{
+                 db.ref('/votes/' +today+ '/' +symbol+ '/upVotes').transaction(function(upVotes) {
+                   return upVotes + 1
+                 });
+               });
+          });
+        } else {
+          console.log("This user already voted on this instrument on this day. Change chart state.")
+        }
+    });
+})
