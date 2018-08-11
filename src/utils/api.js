8361397df54761625745bd4b5c176787cec26641
Micah81 <micahmrocks@gmail.com>
2018-08-11 15:07:48 -0600

    Chart: spinner fetching data. Handle: no sym.Limit API calls w firebase

--- src/utils/api.js
+++ src/utils/api.js
@@ -25,9 +25,24 @@ module.exports = {
           keys.forEach((key, i)=>{
             getQuotes((hash[key]), latestQuotes)
           })
-          return (
-            latestQuotes.reverse()
-          )
+
+          if (latestQuotes){
+            return (
+              latestQuotes.reverse()
+            )
+          } else {
+            return (
+              [
+                {open: 5, close: 10, high: 15, low: 0},
+                {open: 10, close: 15, high: 20, low: 5},
+                {open: 15, close: 20, high: 22, low: 10},
+                {open: 20, close: 10, high: 25, low: 7},
+                {open: 10, close: 8, high: 15, low: 5}
+              ]
+            )
+          }
+
+
         }
       })
   }
